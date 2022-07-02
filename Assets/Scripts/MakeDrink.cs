3b517917ca08b6dcea5de3aaa73153e18aeea8d7
(hagoeun) <(hge020119@sookmyung.ac.kr)>
2022-07-02 19:01:39 +0900

    Add Change Drink Function

--- Assets/Scripts/MakeDrink.cs
+++ Assets/Scripts/MakeDrink.cs
@@ -7,11 +7,10 @@ using UnityEngine.UI;
 
 public class MakeDrink : MonoBehaviour
 {
-    // tag�� ���� ������
     Rigidbody2D cup;
-    // �������� �����ϴ� UI ��Ҹ� ������ 
-    private Image randomDrink;
-    // ��Ḧ �迭�� ����
+    // 랜덤 음료수의 이미지
+    public Sprite randomDrink;
+    // 음료수에 맞는 재료
     public GameObject[] americano;
     public GameObject[] cafeLatte;
     public GameObject[] milkShake;
@@ -20,114 +19,129 @@ public class MakeDrink : MonoBehaviour
     public GameObject[] strawberryShake;
     public GameObject[] vanillaLatte;
     public GameObject[] cafeMocha;
-    // ������� �迭�� ����
     public Sprite[] drink;
-    
-    // ��� ���� ����
-    private int checkIngredient = 0;
 
-    // �ش�Ǵ� ������� Ȯ��
+    // 새로운 음료가 필요한지 확인
+    public bool needNewDrink;
+    
+    // 현재까지 맞게 들어간 재료의 개수 확인
+    private int RightIngredient = 0;
+
+    // 맞는 재료가 들어갔는지 확인
     private bool isRightIngredient;
 
-    // �ʿ��� ����� ����
+    // 필요한 음료의 개수
     private int needIngredient;
+
+    public GameObject other;
 
     void Start()
     {
-        //Rigidbody2D�� ����
+        //Rigidbody2D 요소를 가지고 옴
         cup = GetComponent<Rigidbody2D>();
-        randomDrink = GameObject.FindWithTag("randomDrink").GetComponent<Image>();
+        needNewDrink = true;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        CheckIsRightDrink();
         // Americano: espresso, water 
         if (randomDrink == drink[0])
         {
-            // �浹�� ��ᰡ �迭�� �ִ��� Ȯ��
+            // 배열에 맞는 재료가 있는지 확인
             isRightIngredient = Array.Exists(americano, x => x == other.gameObject);
-            needIngredient = americano.Length;
         }
 
         // Cafe latte: espresso, milk 
         else if (randomDrink == drink[1])
         {
             isRightIngredient = Array.Exists(cafeLatte, x => x == other.gameObject);
-            needIngredient = cafeLatte.Length;
         }
 
         // Milk shake: ice cream, milk, ice
         else if (randomDrink == drink[2])
         {
             isRightIngredient = Array.Exists(milkShake, x => x == other.gameObject);
-            needIngredient = milkShake.Length;
         }
 
         // Cappuccino: espresso, milk, cinnamon
         else if (randomDrink == drink[3])
         {
             isRightIngredient = Array.Exists(cappuccino, x => x == other.gameObject);
-            needIngredient = cappuccino.Length;
         }
 
         // einspanner: water, coffee powder, cream
         else if (randomDrink == drink[4])
         {
             isRightIngredient = Array.Exists(einspanner, x => x == other.gameObject);
-            needIngredient = einspanner.Length;
         }
 
         // Vanilla latte: syrup, espresso, milk, cream
         else if (randomDrink == drink[5])
         {
-            isRightIngredient = Array.Exists(vanillaLatte, x => x == other.gameObject);
-            needIngredient = vanillaLatte.Length;
+            isRightIngredient = Array.Exists(strawberryShake, x => x == other.gameObject);
         }
+        
 
         // Strawberry shake: milk, strawberry, cream, ice
         else if (randomDrink == drink[6])
         {
-            isRightIngredient = Array.Exists(strawberryShake, x => x == other.gameObject);
-            needIngredient = strawberryShake.Length;
+            isRightIngredient = Array.Exists(vanillaLatte, x => x == other.gameObject);
         }
 
         // Mocha latte: syrup, espresso, milk, choco syrup
         else if (randomDrink == drink[7])
         {
             isRightIngredient = Array.Exists(cafeMocha, x => x == other.gameObject);
-            needIngredient = cafeMocha.Length;
         }
 
-        CountIngredient(isRightIngredient);
-        FinishMakingDrinks(needIngredient);
-
-        void CountIngredient(bool check)
-        {
-            other.gameObject.SetActive(false);
-            if (check == true)
-            {
-                checkIngredient += 1;
-            }
-            else
-            {
-                Debug.Log("����");
-            }
-        }
-
-        void FinishMakingDrinks(int ingredient)
-        {
-            if (checkIngredient == ingredient)
-            {
-                Debug.Log("�ϼ�");
-
-            }
-        }
-
+        other.gameObject.SetActive(false);
+        CheckMakingDrink(isRightIngredient, needIngredient);
     }
 
     void Update()
     {
-            // �־��� �ð�����
-            //OnCollisionEnter2D(); ����
+        randomDrink = other.GetComponent<RandomDrink>().GetComponent<SpriteRenderer>().sprite;
     }
+
+    void CheckIsRightDrink()
+    {
+        if (randomDrink == drink[0] || randomDrink == drink[1])
+        {
+            Debug.Log("2");
+            needIngredient = 2;
+        }
+        else if (randomDrink == drink[2] || randomDrink == drink[3] || randomDrink == drink[4])
+        {
+            Debug.Log("3");
+            needIngredient = 3;
+        }
+        else if (randomDrink == drink[5] || randomDrink == drink[6] || randomDrink == drink[7])
+        {
+            Debug.Log("4");
+            needIngredient = 4;
+        }
+    }
+
+    void CheckMakingDrink(bool check, int ingredient)
+    {
+        if (check == true)
+        {
+            RightIngredient += 1;
+            Debug.Log("Right Ingredient");
+
+            if (ingredient == RightIngredient)
+            {
+                Debug.Log("You made drink");
+                RightIngredient = 0;
+                needNewDrink = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Wrong Ingredient");
+            needNewDrink = true;
+        }
+    }
+
 }
--- Assets/Scripts/RandomDrink.cs
+++ Assets/Scripts/RandomDrink.cs
@@ -6,32 +6,35 @@ using UnityEngine.UI;
 
 public class RandomDrink : MonoBehaviour
 {
-    private Image randomDrinkImage;
-
     // Drink array 생성
     public Sprite[] drink;
 
+    // 음료를 다 만들었는지 확인 가능
+    public bool checkDrink;    
+
+    public GameObject other;
+    public GameObject materials;
+
     void Start()
     {
-    // Component 접근 시 Start() 또는 Awake() 함수에서 미리 선언해야 함
-    // 다른 게임 오브젝트에 있는 컴포넌트 접근 시
-    // public GameObject other; 선언 후
-    // tr = other.GetComponent<Transform>();
-    // 또는 Button button = GameObject.FindGameObjectWithTag("게임오브젝트이름").GetComponent<Button>();
-        randomDrinkImage = GetComponent<Image>();
 
     }
 
     void Update()
     {
-        // 8개의 음료수 중 하나가 랜덤으로 나와야 함.
+        IsRightDrink();
     }
 
-    void ShowRandomDrink()
+    void IsRightDrink()
     {
-        // Random을 사용하여 랜덤으로 sprite를 교체해줌.
-        int random = Random.Range(0, 8);
-        randomDrinkImage.sprite = drink[random];
-
+        checkDrink = other.GetComponent<MakeDrink>().needNewDrink;
+        if (checkDrink == true)
+        {
+            other.GetComponent<MakeDrink>().needNewDrink = false;
+            // Random을 사용하여 랜덤으로 sprite를 교체해줌.
+            int random = Random.Range(0, 8);
+            gameObject.GetComponent<SpriteRenderer>().sprite = drink[random];
+        }
     }
+
 }
