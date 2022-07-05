c4e7c6c65f432f1816796c15095d533989f74576
(hagoeun) <(hge020119@sookmyung.ac.kr)>
2022-07-05 13:56:26 +0900

    Modify RandomDrink Script

--- Assets/Scripts/RandomDrink.cs
+++ Assets/Scripts/RandomDrink.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// UI 사용시 필요함
-using UnityEngine.UI;
 
 public class RandomDrink : MonoBehaviour
 {
     public Sprite[] drink;
 
-    // 음료를 다 만들었는지 확인 가능
+    // Check Drink
     public bool checkDrink;
 
     public GameObject other;
@@ -28,10 +26,11 @@ public class RandomDrink : MonoBehaviour
     void IsRightDrink()
     {
         checkDrink = makeDrink.needNewDrink;
+
         if (checkDrink == true)
         {
             makeDrink.needNewDrink = false;
-            // Random을 사용하여 랜덤으로 sprite를 교체해줌.
+            // Change Sprite Randomly
             int random = Random.Range(0, 8);
             gameObject.GetComponent<SpriteRenderer>().sprite = drink[random];
         }
