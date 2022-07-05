68148b61fdd0b3256bc577ee34f738aca5720916
(hagoeun) <(hge020119@sookmyung.ac.kr)>
2022-07-05 13:57:47 +0900

    Modify OnCollisionEnter2D & Add ShowTxt

--- Assets/Scripts/MakeDrink.cs
+++ Assets/Scripts/MakeDrink.cs
@@ -2,150 +2,235 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEngine.UI;
 
 
 public class MakeDrink : MonoBehaviour
 {
-    Rigidbody2D cup;
-
+    public GameObject completeTxt;
+    public GameObject other;
     public Sprite randomDrink;
-    // ingredients
-    public GameObject[] americano;
-    public GameObject[] cafeLatte;
-    public GameObject[] milkShake;
-    public GameObject[] cappuccino;
-    public GameObject[] einspanner;
-    public GameObject[] strawberryShake;
-    public GameObject[] vanillaLatte;
-    public GameObject[] cafeMocha;
-    public GameObject[] materials;
     public Sprite[] drink;
-
     public bool needNewDrink;
-
-    // Check the number of materials that is right
-    private int RightIngredient = 0;
-    private bool isRightIngredient;
-
-    // Check the number of ingredient that is needed
-    private int needIngredient;
+    public int score;
+    
+    private int[] countIngredient;
     private Vector3 materialPos;
     private SpriteRenderer randomDrinkSprite;
 
-    public GameObject other;
-
-    
-
     void Start()
     {
-        cup = GetComponent<Rigidbody2D>();
         randomDrinkSprite = other.GetComponent<RandomDrink>().GetComponent<SpriteRenderer>();
         needNewDrink = true;
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        materialPos = other.gameObject.GetComponent<DragNDrop>().colliderPos;
-        var collisoinObj = other.gameObject;
-
-        CheckIsRightDrink();
-
-        // Americano: espresso, water 
-        if (randomDrink == drink[0])
-        {
-            isRightIngredient = Array.Exists(americano, x => x == other.gameObject);
-        }
-
-        // Cafe latte: espresso, milk 
-        else if (randomDrink == drink[1])
-        {
-            isRightIngredient = Array.Exists(cafeLatte, x => x == other.gameObject);
-        }
-
-        // Milk shake: ice cream, milk, ice
-        else if (randomDrink == drink[2])
-        {
-            isRightIngredient = Array.Exists(milkShake, x => x == other.gameObject);
-        }
-
-        // Cappuccino: espresso, milk, cinnamon
-        else if (randomDrink == drink[3])
-        {
-            isRightIngredient = Array.Exists(cappuccino, x => x == other.gameObject);
-        }
-
-        // einspanner: water, coffee powder, cream
-        else if (randomDrink == drink[4])
-        {
-            isRightIngredient = Array.Exists(einspanner, x => x == other.gameObject);
-        }
-
-        // Vanilla latte: syrup, espresso, milk, cream
-        else if (randomDrink == drink[5])
-        {
-            isRightIngredient = Array.Exists(strawberryShake, x => x == other.gameObject);
-        }
-        
-
-        // Strawberry shake: milk, strawberry, cream, ice
-        else if (randomDrink == drink[6])
-        {
-            isRightIngredient = Array.Exists(vanillaLatte, x => x == other.gameObject);
-        }
-
-        // Mocha latte: syrup, espresso, milk, choco syrup
-        else if (randomDrink == drink[7])
-        {
-            isRightIngredient = Array.Exists(cafeMocha, x => x == other.gameObject);
-        }
-
-        CheckMakingDrink(isRightIngredient, needIngredient);
-        GameObject newMaterial = Instantiate(collisoinObj, materialPos, Quaternion.identity);
-        Destroy(other.gameObject);
+        countIngredient = new int[4] { 0, 0, 0, 0 };
     }
 
     void Update()
     {
         randomDrink = randomDrinkSprite.sprite;
     }
-    void CheckIsRightDrink()
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        materialPos = other.gameObject.GetComponent<DragNDrop>().colliderPos;
+        GameObject collisionObj = other.gameObject;
+
+        Instantiate(collisionObj, materialPos, Quaternion.identity);
+        Destroy(other.gameObject);
+
+        // Americano: espresso, water
+        if (randomDrink == drink[0])
+            switch (collisionObj.tag)
+            {
+                case "Espresso":
+                    countIngredient[0] += 1;
+                    break;
+                case "Water":
+                    countIngredient[1] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        // Cafe latte: espresso, milk 
+        else if (randomDrink == drink[1])
+            switch (collisionObj.tag)
+            {
+                case "Espresso":
+                    countIngredient[0] += 1;
+                    break;
+                case "Milk":
+                    countIngredient[1] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        // Milk shake: ice cream, milk, ice
+        else if (randomDrink == drink[2])
+            switch (collisionObj.tag)
+            {
+                case "IceCream":
+                    countIngredient[0] += 1;
+                    break;
+                case "Milk":
+                    countIngredient[1] += 1;
+                    break;
+                case "IceBox":
+                    countIngredient[2] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        // Cappuccino: espresso, milk, cinnamon
+        else if (randomDrink == drink[3])
+            switch (collisionObj.tag)
+            {
+                case "Espresso":
+                    countIngredient[0] += 1;
+                    break;
+                case "Milk":
+                    countIngredient[1] += 1;
+                    break;
+                case "Cinnamon":
+                    countIngredient[2] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        // einspanner: water, coffee powder, cream
+        else if (randomDrink == drink[4])
+            switch (collisionObj.tag)
+            {
+                case "Water":
+                    countIngredient[0] += 1;
+                    break;
+                case "CoffeePowder":
+                    countIngredient[1] += 1;
+                    break;
+                case "Cream":
+                    countIngredient[2] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        // Strawberry shake: milk, strawberry, cream, ice
+        else if (randomDrink == drink[5])
+            switch (collisionObj.tag)
+            {
+                case "Milk":
+                    countIngredient[0] += 1;
+                    break;
+                case "Strawberry":
+                    countIngredient[1] += 1;
+                    break;
+                case "Cream":
+                    countIngredient[2] += 1;
+                    break;
+                case "IceBox":
+                    countIngredient[3] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        // Vanilla latte: syrup, espresso, milk, cream
+        else if (randomDrink == drink[6])
+            switch (collisionObj.tag)
+            {
+                case "Syrup":
+                    countIngredient[0] += 1;
+                    break;
+                case "Espresso":
+                    countIngredient[1] += 1;
+                    break;
+                case "Milk":
+                    countIngredient[2] += 1;
+                    break;
+                case "Cream":
+                    countIngredient[3] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+        // Cafe Mocha: syrup, espresso, milk, choco syrup
+        else if (randomDrink == drink[7])
+            switch (collisionObj.tag)
+            {
+                case "Syrup":
+                    countIngredient[0] += 1;
+                    break;
+                case "Espresso":
+                    countIngredient[1] += 1;
+                    break;
+                case "Milk":
+                    countIngredient[2] += 1;
+                    break;
+                case "ChocolateSyrup":
+                    countIngredient[3] += 1;
+                    break;
+                default:
+                    needNewDrink = true;
+                    break;
+            }
+
+        CheckMakingDrink();
+    }
+
+    void CheckMakingDrink()
     {
         if (randomDrink == drink[0] || randomDrink == drink[1])
         {
-            Debug.Log("2");
-            needIngredient = 2;
-        }
-        else if (randomDrink == drink[2] || randomDrink == drink[3] || randomDrink == drink[4])
-        {
-            Debug.Log("3");
-            needIngredient = 3;
-        }
-        else if (randomDrink == drink[5] || randomDrink == drink[6] || randomDrink == drink[7])
-        {
-            Debug.Log("4");
-            needIngredient = 4;
-        }
-    }
-
-    void CheckMakingDrink(bool check, int ingredient)
-    {
-        if (check == true)
-        {
-            RightIngredient += 1;
-            Debug.Log("Right Ingredient");
-
-            if (ingredient == RightIngredient)
+            if (countIngredient[0] == 1 && countIngredient[1] == 1)
             {
-                Debug.Log("You made drink");
-                RightIngredient = 0;
                 needNewDrink = true;
+                score -= 1;
+                StartCoroutine(ShowTxt());
+                
             }
         }
-        else
+        else if ((randomDrink == drink[2]) || (randomDrink == drink[3]) || (randomDrink == drink[4]))
         {
-            Debug.Log("Wrong Ingredient");
-            needNewDrink = true;
+            if ((countIngredient[0] == 1) && (countIngredient[1] == 1) && (countIngredient[2] == 1))
+            {
+                needNewDrink = true;
+                score -= 1;
+                StartCoroutine(ShowTxt());
+                
+            }
         }
+        else if ((randomDrink == drink[5]) || (randomDrink == drink[6]) || (randomDrink == drink[7]))
+        {
+            if ((countIngredient[0] == 1) && (countIngredient[1] == 1) && (countIngredient[2] == 1) && (countIngredient[3] == 1))
+            {
+                needNewDrink = true;
+                score -= 1;
+                StartCoroutine(ShowTxt());
+                
+            }
+        }
+
+        if (countIngredient[0] > 1 || countIngredient[1] > 1 || countIngredient[2] > 1 || countIngredient[3] > 1)
+            needNewDrink = true;
+
+        if (needNewDrink == true)
+            countIngredient = new int[4] { 0, 0, 0, 0 };
     }
+
+    IEnumerator ShowTxt()
+    {
+        completeTxt.SetActive(true);
+        yield return new WaitForSeconds(0.4f);
+        completeTxt.SetActive(false);
+    }
+    
 }
